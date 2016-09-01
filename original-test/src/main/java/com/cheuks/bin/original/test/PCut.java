8d86b51ac7cc34eed5e9e5b94dae8f996e59220d
bin <20796698@qq.com>
2016-09-01 23:12:17 +0800

    a

---
+++ original-test/src/main/java/com/cheuks/bin/original/test/PCut.java
@@ -0,0 +1,13 @@
+package com.cheuks.bin.original.test;
+
+public class PCut {
+
+	public void before() {
+		System.err.println("before");
+	}
+
+	public void after() {
+		System.err.println("after");
+	}
+
+}
