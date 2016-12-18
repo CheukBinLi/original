54c68c85a8f1e9d53e77a6bde383660fa1874ca7
bin <20796698@qq.com>
2016-12-18 21:56:52 +0800

    a

--- original-reflect/src/main/java/com/cheuks/bin/original/reflect/rmi/SimpleRmiClient.java
+++ original-reflect/src/main/java/com/cheuks/bin/original/reflect/rmi/SimpleRmiClient.java
@@ -1,15 +1,8 @@
 package com.cheuks.bin.original.reflect.rmi;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.cheuks.bin.original.reflect.test2I;
 import com.cheuks.bin.original.reflect.rmi.model.TransmissionModel;
 import com.cheuks.bin.original.reflect.rmi.net.HandleType;
 import com.cheuks.bin.original.reflect.rmi.net.netty.NettyClientHandle;
