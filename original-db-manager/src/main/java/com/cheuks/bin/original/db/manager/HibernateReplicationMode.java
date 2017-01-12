c845db1d0c2dd5d3d847f7ceb3625273e984fdff
BenPack <20796698@qq.com>
2017-01-13 00:18:45 +0800

    a

--- original-db-manager/src/main/java/com/cheuks/bin/original/db/manager/HibernateReplicationMode.java
+++
@@ -1,8 +0,0 @@
-package com.cheuks.bin.original.db.manager;
-
-public interface HibernateReplicationMode {
-	final String EXCEPTION = "EXCEPTION";
-	final String IGNORE = "IGNORE";
-	final String OVERWRITE = "OVERWRITE";
-	final String LATEST_VERSION = "LATEST_VERSION";
-}
