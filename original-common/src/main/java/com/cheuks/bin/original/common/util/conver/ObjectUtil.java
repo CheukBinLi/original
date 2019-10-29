2d55f290afdfe75a1e9720a4262c05abbdf453e3
bin <20796698@qq.com>
2019-10-29 16:07:34 +0800

    update

---
+++ original-common/src/main/java/com/cheuks/bin/original/common/util/conver/ObjectUtil.java
@@ -0,0 +1,52 @@
+package com.cheuks.bin.original.common.util.conver;
+
+public class ObjectUtil {
+
+    public static <T extends Object> T defaultNull(T t, T defValue) {
+        return null == t ? defValue : t;
+    }
+
+    public static boolean anyNull(Object... params) {
+        return !anyNotNull(params);
+    }
+
+    public static boolean anyNotNull(Object... params) {
+        if (null == params || params.length < 1) {
+            return false;
+        }
+        for (Object item : params) {
+            if (null == item)
+                return false;
+        }
+        return true;
+    }
+
+    public static boolean equals(Object target, Object... params) {
+        if (params == null)
+            return target == null;
+        for (Object item : params) {
+            if (null == item) {
+                if (target == null) {
+                    return true;
+                }
+            } else {
+                if (item.equals(target))
+                    return true;
+            }
+
+        }
+        return false;
+    }
+
+    public static boolean equals(int target, Integer... params) {
+        if (null == params)
+            return false;
+        for (int item : params) {
+            if (target == item)
+                return true;
+        }
+        return false;
+    }
+
+
+}
